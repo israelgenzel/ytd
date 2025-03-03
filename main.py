441ca0e5fab8eb7bff7e71ce6c73cca27e957b57
israelgenzel <34340836+israelgenzel@users.noreply.github.com>
2025-03-03 09:59:38 +0200

    4

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, send_file, jsonify
+from flask import Flask, request, send_file, jsonify, after_this_request
 import yt_dlp
 import os
+import time
 
 app = Flask(__name__)
 
@@ -17,7 +18,7 @@ def download_video():
 
     try:
         ydl_opts = {
-            'format': 'bestvideo/best',  # אפשר לשנות לפי הצורך
+            'format': 'bestvideo/best',
             'outtmpl': os.path.join(DOWNLOAD_FOLDER, '%(title)s.%(ext)s'),
         }
 
@@ -25,19 +26,31 @@ def download_video():
             ydl.download([video_url])
 
         # מציאת הקובץ שהורד
-        downloaded_files = os.listdir("downloads")
+        downloaded_files = os.listdir(DOWNLOAD_FOLDER)
         print(downloaded_files)
         if not downloaded_files:
             return jsonify({"error": "Download failed"}), 500
 
         print(f"✅ הורדה הושלמה: {downloaded_files[0]}")
         filename = os.path.join(DOWNLOAD_FOLDER, downloaded_files[0])
+
+        # מחיקת הקובץ אחרי שליחה
+        @after_this_request
+        def cleanup(response):
+            try:
+                if os.path.exists(filename):
+                    time.sleep(1)  # חכה שנייה לוודא שהשליחה הסתיימה
+                    os.remove(filename)
+                    print(f"🗑️ קובץ נמחק: {filename}")
+            except Exception as e:
+                print(f"⚠️ שגיאה במחיקה: {e}")
+            return response
+
         return send_file(filename, as_attachment=True)
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
-
-
